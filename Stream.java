76b227e30a8f8162145b67b4776b860845591dca
junheeyoon <enteryjh@ajou.ac.kr>
2021-06-18 16:14:13 +0900

    Create Stream.java

---
+++ Stream.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printMemberCountGroupByHobby();
+		printLikeCount();
+	}
+	
+	public static void printMemberCountGroupByHobby() { 
+		List<String> l1 = new ArrayList<String>(Arrays.asList("김프로", "축구:농구:야구", "구기종목 좋아요"));
+		List<String> l2 = new ArrayList<String>(Arrays.asList("정프로", "개발:당구:족구", "개발하는데 뛰긴 싫어"));
+		List<String> l3 = new ArrayList<String>(Arrays.asList("앙몬드", "피아노", "죠르디가 좋아요 좋아좋아너무좋아"));
+		List<String> l4 = new ArrayList<String>(Arrays.asList("죠르디", "스포츠댄스:개발", "개발하는 죠르디"));
+		List<List<String>> persons = new ArrayList<List<String>>();
+		persons.add(l1);
+		persons.add(l2);
+		persons.add(l3);
+		persons.add(l4);
+		//persons.remove(0); 
+		Map<String, Integer> result = new HashMap<>(); 
+		persons.stream().flatMap(member -> Arrays.stream(member.get(1).split(":"))).forEach(hobby -> result.merge(hobby, 1, (oldValue, newValue)->++oldValue)); 
+		result.entrySet().forEach(entry-> System.out.println(entry.getKey() + " " + entry.getValue())); 
+
+	}
+	
+	public static void printLikeCount() {
+		List<String> l1 = new ArrayList<String>(Arrays.asList("김프로", "축구:농구:야구", "구기종목 좋아요"));
+		List<String> l2 = new ArrayList<String>(Arrays.asList("정프로", "개발:당구:족구", "개발하는데 뛰긴 싫어"));
+		List<String> l3 = new ArrayList<String>(Arrays.asList("앙몬드", "피아노", "죠르디가 좋아요 좋아좋아너무좋아"));
+		List<String> l4 = new ArrayList<String>(Arrays.asList("죠르디", "스포츠댄스:개발", "개발하는 죠르디"));
+		List<List<String>> lines = new ArrayList<List<String>>();
+		lines.add(l1);
+		lines.add(l2);
+		lines.add(l3);
+		lines.add(l4); 
+		
+		final String word = "좋아"; 
+		int result = lines.stream().map(member -> countFindString(member.get(2), word)).reduce(0, Integer::sum);
+		System.out.println(word+" "+result); 
+	} 
+	private static int countFindString(String source, String target){ 
+		int idx = source.indexOf(target); 
+		if(idx == -1){ 
+			return 0; 
+		}else{ 
+			return 1 + countFindString(source.substring(idx + 2), target);
+		} 
+	}	
+
+}
