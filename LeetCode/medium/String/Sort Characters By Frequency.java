b04f7daa241c12ab9838750104e721d16b6ad67b
junheeyoon <enteryjh@ajou.ac.kr>
2021-08-26 12:08:28 +0900

    Create Sort Characters By Frequency.java

---
+++ LeetCode/medium/String/Sort Characters By Frequency.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();        
+        String result = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        
+        List<Character> keyList = new ArrayList<>(map.keySet());
+        
+        Collections.sort(keyList, (o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+        
+        for(Character a: keyList){
+            String str = Character.toString(a);
+            result += str.repeat(map.get(a));
+        }
+        
+        return result;
+    }
+}
